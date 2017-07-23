e602e560ac4614e67513029dba6b95ab793db9c9
Kate Catlin <kcatlin2@gmail.com>
2017-07-23 16:14:38 -0600

    Adding intent to 2nd screen

--- app/src/main/java/com/example/katecatlin/womenrisingandroid/LoginActivity.java
+++ app/src/main/java/com/example/katecatlin/womenrisingandroid/LoginActivity.java
@@ -1,7 +1,13 @@
 package com.example.katecatlin.womenrisingandroid;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +15,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ImageButton loginButton = (ImageButton) findViewById(R.id.LILoginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ProfileViewActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
---
+++ app/src/main/java/com/example/katecatlin/womenrisingandroid/ProfileViewActivity.java
@@ -0,0 +1,8 @@
+package com.example.katecatlin.womenrisingandroid;
+
+/**
+ * Created by katecatlin on 7/23/17.
+ */
+
+public class ProfileViewActivity {
+}
