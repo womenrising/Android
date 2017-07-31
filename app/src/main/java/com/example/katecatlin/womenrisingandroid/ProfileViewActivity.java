18cc22dc1759d1ad5b4b31ad0790fd9c0810a1d7
Kate Catlin <kcatlin2@gmail.com>
2017-07-30 21:19:43 -0600

    Making ProfileViewActivity into an actual activity

--- app/src/main/java/com/example/katecatlin/womenrisingandroid/ProfileViewActivity.java
+++ app/src/main/java/com/example/katecatlin/womenrisingandroid/ProfileViewActivity.java
@@ -1,6 +1,10 @@
 package com.example.katecatlin.womenrisingandroid;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by katecatlin on 7/23/17.
@@ -8,7 +12,12 @@ import android.app.Activity;
 
 public class ProfileViewActivity extends Activity {
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profileview);
+        
+    }
 
 
 }
