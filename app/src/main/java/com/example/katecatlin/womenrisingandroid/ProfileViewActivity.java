caca43bd886bd65f49b6caa29b044cb1e7c0995a
Kate Catlin <kcatlin2@gmail.com>
2017-07-30 21:23:19 -0600

    And adding aProfileViewActivity to the manifest

--- app/src/main/java/com/example/katecatlin/womenrisingandroid/ProfileViewActivity.java
+++ app/src/main/java/com/example/katecatlin/womenrisingandroid/ProfileViewActivity.java
@@ -16,7 +16,7 @@ public class ProfileViewActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profileview);
-        
+
     }
 
 
